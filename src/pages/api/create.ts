a894e1bd501cb07db0efb3bbd7f162b4748a88c5
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-09-17 16:06:38 +0900

    記事投稿のAPIをRoute HandlersからApp Routerに変更

--- src/pages/api/create.ts
+++ src/pages/api/create.ts
@@ -1,3 +1,6 @@
+/*
+App Routerを使わない書き方（Route Handlers）
+
 import { supabase } from "@/utils/supabaseClients"
 import { NextApiRequest, NextApiResponse } from "next"
 
@@ -17,3 +20,4 @@ export default async function handler(
 
   return res.status(200).json(data)
 }
+ */
